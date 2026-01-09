5c8c1838d7f5f68a831c4a4ceb942be474facbef
Zaid Arath <angieillumi@gmail.com>
2026-01-09 14:12:50 -0600

    test(presence): update v0.3 tests for v0.4 compatibility
    - Update constructor to 3-argument signature (epochRegistry, validatorRegistry, disputeWindow)
    - Add ValidatorRegistry setup with 3 validators
    - Update finalization flow: Declared -> Validated -> Finalized
    - Add ghost_validated tracking in handler
    - Update invariants for extended state space

--- test/v0.3/presence/Declaration.t.sol
+++ test/v0.3/presence/Declaration.t.sol
@@ -7,6 +7,8 @@ import {PresenceRegistry} from "../../../contracts/core/PresenceRegistry.sol";
 import {IPresenceRegistry} from "../../../contracts/interfaces/IPresenceRegistry.sol";
 import {EpochRegistry} from "../../../contracts/core/EpochRegistry.sol";
 import {IEpochRegistry} from "../../../contracts/interfaces/IEpochRegistry.sol";
+import {ValidatorRegistry} from "../../../contracts/core/ValidatorRegistry.sol";
+import {IValidatorRegistry} from "../../../contracts/interfaces/IValidatorRegistry.sol";
 
 /**
  * @title PresenceRegistryDeclarationTests
@@ -16,16 +18,25 @@ import {IEpochRegistry} from "../../../contracts/interfaces/IEpochRegistry.sol";
 contract PresenceRegistryDeclarationTests is Test {
     IPresenceRegistry internal registry;
     IEpochRegistry internal epochRegistry;
+    IValidatorRegistry internal validatorRegistry;
     address internal constant AUTHORITY = address(0xA077);
     address internal constant ACTOR = address(0xA11CE);
+    address internal constant VALIDATOR_1 = address(0x1001);
+    address internal constant VALIDATOR_2 = address(0x1002);
+    address internal constant VALIDATOR_3 = address(0x1003);
     uint256 internal constant EPOCH_ID = 1;
 
     function setUp() external {
         epochRegistry = IEpochRegistry(address(new EpochRegistry(AUTHORITY)));
-        registry = IPresenceRegistry(address(new PresenceRegistry(epochRegistry)));
+        validatorRegistry = IValidatorRegistry(address(new ValidatorRegistry(AUTHORITY)));
+        registry = IPresenceRegistry(address(new PresenceRegistry(epochRegistry, validatorRegistry, 0)));
 
-        vm.prank(AUTHORITY);
+        vm.startPrank(AUTHORITY);
         epochRegistry.createEpoch(EPOCH_ID, block.timestamp, block.timestamp + 1 days);
+        validatorRegistry.addValidator(VALIDATOR_1);
+        validatorRegistry.addValidator(VALIDATOR_2);
+        validatorRegistry.addValidator(VALIDATOR_3);
+        vm.stopPrank();
     }
 
     /*//////////////////////////////////////////////////////////////
@@ -34,7 +45,12 @@ contract PresenceRegistryDeclarationTests is Test {
 
     function test_constructorRejectsZeroEpochRegistry() external {
         vm.expectRevert(IPresenceRegistry.InvalidEpochRegistry.selector);
-        new PresenceRegistry(IEpochRegistry(address(0)));
+        new PresenceRegistry(IEpochRegistry(address(0)), validatorRegistry, 0);
+    }
+
+    function test_constructorRejectsZeroValidatorRegistry() external {
+        vm.expectRevert(IPresenceRegistry.InvalidValidatorRegistry.selector);
+        new PresenceRegistry(epochRegistry, IValidatorRegistry(address(0)), 0);
     }
 
     /*//////////////////////////////////////////////////////////////
@@ -76,9 +92,19 @@ contract PresenceRegistryDeclarationTests is Test {
         assertEq(uint256(registry.presenceState(ACTOR, EPOCH_ID)), uint256(IPresenceRegistry.PresenceState.Declared));
     }
 
-    function test_declarePresence_idempotent_whenFinalized() external {
+    function test_declarePresence_idempotent_whenValidated() external {
+        // Declare and validate to Validated state
         vm.prank(ACTOR);
-        registry.finalizePresence(ACTOR, EPOCH_ID);
+        registry.declarePresence(ACTOR, EPOCH_ID);
+
+        vm.prank(VALIDATOR_1);
+        registry.validatePresence(ACTOR, EPOCH_ID);
+        vm.prank(VALIDATOR_2);
+        registry.validatePresence(ACTOR, EPOCH_ID);
+        vm.prank(VALIDATOR_3);
+        registry.validatePresence(ACTOR, EPOCH_ID);
+
+        assertEq(uint256(registry.presenceState(ACTOR, EPOCH_ID)), uint256(IPresenceRegistry.PresenceState.Validated));
 
         vm.recordLogs();
         vm.prank(ACTOR);
@@ -87,7 +113,7 @@ contract PresenceRegistryDeclarationTests is Test {
         Vm.Log[] memory logs = vm.getRecordedLogs();
         assertEq(logs.length, 0);
 
-        assertEq(uint256(registry.presenceState(ACTOR, EPOCH_ID)), uint256(IPresenceRegistry.PresenceState.Finalized));
+        assertEq(uint256(registry.presenceState(ACTOR, EPOCH_ID)), uint256(IPresenceRegistry.PresenceState.Validated));
     }
 
     /*//////////////////////////////////////////////////////////////
@@ -140,30 +166,57 @@ contract PresenceRegistryDeclarationTests is Test {
     }
 
     /*//////////////////////////////////////////////////////////////
-                            FINALIZATION FROM DECLARED
+                            FINALIZATION (v0.4 FLOW)
     //////////////////////////////////////////////////////////////*/
 
-    function test_finalizePresence_fromDeclared() external {
+    function test_finalizePresence_fromValidated() external {
+        // Declare
         vm.prank(ACTOR);
         registry.declarePresence(ACTOR, EPOCH_ID);
-        assertEq(uint256(registry.presenceState(ACTOR, EPOCH_ID)), uint256(IPresenceRegistry.PresenceState.Declared));
 
-        vm.prank(ACTOR);
-        registry.finalizePresence(ACTOR, EPOCH_ID);
-        assertEq(uint256(registry.presenceState(ACTOR, EPOCH_ID)), uint256(IPresenceRegistry.PresenceState.Finalized));
-    }
+        // Validate with quorum
+        vm.prank(VALIDATOR_1);
+        registry.validatePresence(ACTOR, EPOCH_ID);
+        vm.prank(VALIDATOR_2);
+        registry.validatePresence(ACTOR, EPOCH_ID);
+        vm.prank(VALIDATOR_3);
+        registry.validatePresence(ACTOR, EPOCH_ID);
 
-    function test_finalizePresence_fromNone_legacy() external {
-        vm.prank(ACTOR);
-        registry.finalizePresence(ACTOR, EPOCH_ID);
-        assertEq(uint256(registry.presenceState(ACTOR, EPOCH_ID)), uint256(IPresenceRegistry.PresenceState.Finalized));
-    }
+        assertEq(uint256(registry.presenceState(ACTOR, EPOCH_ID)), uint256(IPresenceRegistry.PresenceState.Validated));
 
-    function test_finalizePresence_reverts_epochNotActive() external {
+        // Warp to Closed epoch
         vm.warp(block.timestamp + 2 days);
 
+        // Finalize
+        registry.finalizePresence(ACTOR, EPOCH_ID);
+        assertEq(uint256(registry.presenceState(ACTOR, EPOCH_ID)), uint256(IPresenceRegistry.PresenceState.Finalized));
+    }
+
+    function test_finalizePresence_reverts_notValidated() external {
         vm.prank(ACTOR);
-        vm.expectRevert(abi.encodeWithSelector(IPresenceRegistry.EpochNotActive.selector, EPOCH_ID));
+        registry.declarePresence(ACTOR, EPOCH_ID);
+
+        // Warp to Closed epoch
+        vm.warp(block.timestamp + 2 days);
+
+        vm.expectRevert(abi.encodeWithSelector(IPresenceRegistry.PresenceNotFinalizable.selector, ACTOR, EPOCH_ID));
+        registry.finalizePresence(ACTOR, EPOCH_ID);
+    }
+
+    function test_finalizePresence_reverts_epochStillActive() external {
+        // Declare and validate
+        vm.prank(ACTOR);
+        registry.declarePresence(ACTOR, EPOCH_ID);
+
+        vm.prank(VALIDATOR_1);
+        registry.validatePresence(ACTOR, EPOCH_ID);
+        vm.prank(VALIDATOR_2);
+        registry.validatePresence(ACTOR, EPOCH_ID);
+        vm.prank(VALIDATOR_3);
+        registry.validatePresence(ACTOR, EPOCH_ID);
+
+        // Try to finalize while epoch is still active
+        vm.expectRevert(abi.encodeWithSelector(IPresenceRegistry.PresenceNotFinalizable.selector, ACTOR, EPOCH_ID));
         registry.finalizePresence(ACTOR, EPOCH_ID);
     }
 
@@ -185,7 +238,7 @@ contract PresenceRegistryDeclarationTests is Test {
         assertEq(uint256(registry.presenceState(actor, epochId)), uint256(IPresenceRegistry.PresenceState.Declared));
     }
 
-    function testFuzz_declareAndFinalize(address actor, uint256 epochId) external {
+    function testFuzz_declareAndValidate(address actor, uint256 epochId) external {
         vm.assume(actor != address(0));
         vm.assume(epochId != 0);
         vm.assume(epochId != EPOCH_ID);
@@ -197,9 +250,15 @@ contract PresenceRegistryDeclarationTests is Test {
         registry.declarePresence(actor, epochId);
         assertEq(uint256(registry.presenceState(actor, epochId)), uint256(IPresenceRegistry.PresenceState.Declared));
 
-        vm.prank(actor);
-        registry.finalizePresence(actor, epochId);
-        assertEq(uint256(registry.presenceState(actor, epochId)), uint256(IPresenceRegistry.PresenceState.Finalized));
+        // Validate with quorum
+        vm.prank(VALIDATOR_1);
+        registry.validatePresence(actor, epochId);
+        vm.prank(VALIDATOR_2);
+        registry.validatePresence(actor, epochId);
+        vm.prank(VALIDATOR_3);
+        registry.validatePresence(actor, epochId);
+
+        assertEq(uint256(registry.presenceState(actor, epochId)), uint256(IPresenceRegistry.PresenceState.Validated));
     }
 
     /*//////////////////////////////////////////////////////////////
--- test/v0.3/presence/Invariants.t.sol
+++ test/v0.3/presence/Invariants.t.sol
@@ -7,18 +7,25 @@ import {PresenceRegistry} from "../../../contracts/core/PresenceRegistry.sol";
 import {IPresenceRegistry} from "../../../contracts/interfaces/IPresenceRegistry.sol";
 import {EpochRegistry} from "../../../contracts/core/EpochRegistry.sol";
 import {IEpochRegistry} from "../../../contracts/interfaces/IEpochRegistry.sol";
+import {ValidatorRegistry} from "../../../contracts/core/ValidatorRegistry.sol";
+import {IValidatorRegistry} from "../../../contracts/interfaces/IValidatorRegistry.sol";
 
 /**
  * @title PresenceRegistryHandler
- * @notice Handler contract for Foundry invariant testing
- * @dev Spec: specs/v0.3/presence.md
+ * @notice Handler contract for Foundry invariant testing (updated for v0.4)
+ * @dev Spec: specs/v0.4/presence.md
  */
 contract PresenceRegistryHandler is Test {
     IPresenceRegistry public registry;
     IEpochRegistry public epochRegistry;
+    IValidatorRegistry public validatorRegistry;
     address public constant AUTHORITY = address(0xA077);
+    address public constant VALIDATOR_1 = address(0x1001);
+    address public constant VALIDATOR_2 = address(0x1002);
+    address public constant VALIDATOR_3 = address(0x1003);
 
     mapping(address => mapping(uint256 => bool)) public ghost_declared;
+    mapping(address => mapping(uint256 => bool)) public ghost_validated;
     mapping(address => mapping(uint256 => bool)) public ghost_finalized;
     address[] public ghost_actors;
     uint256[] public ghost_epochs;
@@ -26,9 +33,10 @@ contract PresenceRegistryHandler is Test {
     address[] internal boundedActors;
     uint256[] internal boundedEpochs;
 
-    constructor(IPresenceRegistry _registry, IEpochRegistry _epochRegistry) {
+    constructor(IPresenceRegistry _registry, IEpochRegistry _epochRegistry, IValidatorRegistry _validatorRegistry) {
         registry = _registry;
         epochRegistry = _epochRegistry;
+        validatorRegistry = _validatorRegistry;
 
         boundedActors.push(address(0xA11CE));
         boundedActors.push(address(0xB0B));
@@ -44,38 +52,64 @@ contract PresenceRegistryHandler is Test {
         uint256 epochId = boundedEpochs[epochSeed % boundedEpochs.length];
 
         bool wasDeclared = ghost_declared[actor][epochId];
+        bool wasValidated = ghost_validated[actor][epochId];
         bool wasFinalized = ghost_finalized[actor][epochId];
 
         vm.prank(actor);
         registry.declarePresence(actor, epochId);
 
-        if (!wasDeclared && !wasFinalized) {
+        if (!wasDeclared && !wasValidated && !wasFinalized) {
             ghost_declared[actor][epochId] = true;
             ghost_actors.push(actor);
             ghost_epochs.push(epochId);
         }
     }
 
+    function validatePresence(uint256 actorSeed, uint256 epochSeed) external {
+        address actor = boundedActors[actorSeed % boundedActors.length];
+        uint256 epochId = boundedEpochs[epochSeed % boundedEpochs.length];
+
+        // Must be declared first
+        if (!ghost_declared[actor][epochId]) return;
+        if (ghost_validated[actor][epochId]) return;
+        if (ghost_finalized[actor][epochId]) return;
+
+        // Validate with quorum
+        vm.prank(VALIDATOR_1);
+        try registry.validatePresence(actor, epochId) {} catch { return; }
+
+        vm.prank(VALIDATOR_2);
+        try registry.validatePresence(actor, epochId) {} catch { return; }
+
+        vm.prank(VALIDATOR_3);
+        try registry.validatePresence(actor, epochId) {} catch { return; }
+
+        ghost_validated[actor][epochId] = true;
+    }
+
     function finalizePresence(uint256 actorSeed, uint256 epochSeed) external {
         address actor = boundedActors[actorSeed % boundedActors.length];
         uint256 epochId = boundedEpochs[epochSeed % boundedEpochs.length];
 
-        bool wasDeclared = ghost_declared[actor][epochId];
+        bool wasValidated = ghost_validated[actor][epochId];
         bool wasFinalized = ghost_finalized[actor][epochId];
 
-        vm.prank(actor);
-        registry.finalizePresence(actor, epochId);
+        // v0.4: Must be validated first
+        if (!wasValidated) return;
+        if (wasFinalized) return;
 
-        if (!wasFinalized) {
-            ghost_finalized[actor][epochId] = true;
-            if (!wasDeclared) {
-                ghost_actors.push(actor);
-                ghost_epochs.push(epochId);
-            }
+        // Warp to closed epoch if needed
+        IEpochRegistry.EpochState epochState = epochRegistry.epochState(epochId);
+        if (epochState == IEpochRegistry.EpochState.Active) {
+            vm.warp(block.timestamp + 2 days);
         }
+
+        try registry.finalizePresence(actor, epochId) {
+            ghost_finalized[actor][epochId] = true;
+        } catch {}
     }
 
-    function finalizePresenceUnauthorized(uint256 callerSeed, uint256 actorSeed, uint256 epochSeed) external {
+    function declarePresenceUnauthorized(uint256 callerSeed, uint256 actorSeed, uint256 epochSeed) external {
         address caller = boundedActors[callerSeed % boundedActors.length];
         address actor = boundedActors[actorSeed % boundedActors.length];
         uint256 epochId = boundedEpochs[epochSeed % boundedEpochs.length];
@@ -84,7 +118,7 @@ contract PresenceRegistryHandler is Test {
 
         vm.prank(caller);
         vm.expectRevert();
-        registry.finalizePresence(actor, epochId);
+        registry.declarePresence(actor, epochId);
     }
 
     function getGhostActorsLength() external view returns (uint256) {
@@ -102,18 +136,23 @@ contract PresenceRegistryHandler is Test {
 
 /**
  * @title PresenceRegistryInvariants
- * @notice Protocol invariants for presence lifecycle
- * @dev Spec: specs/v0.3/presence.md
+ * @notice Protocol invariants for presence lifecycle (updated for v0.4)
+ * @dev Spec: specs/v0.4/presence.md
  */
 contract PresenceRegistryInvariants is Test {
     IPresenceRegistry internal registry;
     IEpochRegistry internal epochRegistry;
+    IValidatorRegistry internal validatorRegistry;
     PresenceRegistryHandler internal handler;
     address internal constant AUTHORITY = address(0xA077);
+    address internal constant VALIDATOR_1 = address(0x1001);
+    address internal constant VALIDATOR_2 = address(0x1002);
+    address internal constant VALIDATOR_3 = address(0x1003);
 
     function setUp() external {
         epochRegistry = IEpochRegistry(address(new EpochRegistry(AUTHORITY)));
-        registry = IPresenceRegistry(address(new PresenceRegistry(epochRegistry)));
+        validatorRegistry = IValidatorRegistry(address(new ValidatorRegistry(AUTHORITY)));
+        registry = IPresenceRegistry(address(new PresenceRegistry(epochRegistry, validatorRegistry, 0)));
 
         uint256 start = block.timestamp;
         uint256 end = block.timestamp + 1 days;
@@ -121,20 +160,24 @@ contract PresenceRegistryInvariants is Test {
         epochRegistry.createEpoch(1, start, end);
         epochRegistry.createEpoch(2, start, end);
         epochRegistry.createEpoch(3, start, end);
+        validatorRegistry.addValidator(VALIDATOR_1);
+        validatorRegistry.addValidator(VALIDATOR_2);
+        validatorRegistry.addValidator(VALIDATOR_3);
         vm.stopPrank();
 
-        handler = new PresenceRegistryHandler(registry, epochRegistry);
+        handler = new PresenceRegistryHandler(registry, epochRegistry, validatorRegistry);
 
         targetContract(address(handler));
 
-        bytes4[] memory selectors = new bytes4[](3);
+        bytes4[] memory selectors = new bytes4[](4);
         selectors[0] = PresenceRegistryHandler.declarePresence.selector;
-        selectors[1] = PresenceRegistryHandler.finalizePresence.selector;
-        selectors[2] = PresenceRegistryHandler.finalizePresenceUnauthorized.selector;
+        selectors[1] = PresenceRegistryHandler.validatePresence.selector;
+        selectors[2] = PresenceRegistryHandler.finalizePresence.selector;
+        selectors[3] = PresenceRegistryHandler.declarePresenceUnauthorized.selector;
         targetSelector(FuzzSelector({addr: address(handler), selectors: selectors}));
     }
 
-    /// @dev INV1: Valid state space {None, Declared, Finalized}
+    /// @dev INV1: Valid state space {None, Declared, Validated, Finalized, Slashed}
     function invariant_validStateSpace() external view {
         uint256 len = handler.getGhostActorsLength();
         for (uint256 i = 0; i < len; i++) {
@@ -145,7 +188,9 @@ contract PresenceRegistryInvariants is Test {
 
             assertTrue(
                 state == IPresenceRegistry.PresenceState.None || state == IPresenceRegistry.PresenceState.Declared
-                    || state == IPresenceRegistry.PresenceState.Finalized,
+                    || state == IPresenceRegistry.PresenceState.Validated
+                    || state == IPresenceRegistry.PresenceState.Finalized
+                    || state == IPresenceRegistry.PresenceState.Slashed,
                 "INV1: Invalid state"
             );
         }
@@ -162,7 +207,9 @@ contract PresenceRegistryInvariants is Test {
                 IPresenceRegistry.PresenceState state = registry.presenceState(actor, epochId);
                 assertTrue(
                     state == IPresenceRegistry.PresenceState.Declared
-                        || state == IPresenceRegistry.PresenceState.Finalized,
+                        || state == IPresenceRegistry.PresenceState.Validated
+                        || state == IPresenceRegistry.PresenceState.Finalized
+                        || state == IPresenceRegistry.PresenceState.Slashed,
                     "INV2: Declared reverted to None"
                 );
             }
@@ -211,7 +258,10 @@ contract PresenceRegistryInvariants is Test {
 
             uint256 adjacentEpoch = epochId == type(uint256).max ? epochId - 1 : epochId + 1;
 
-            if (!handler.ghost_finalized(actor, adjacentEpoch) && !handler.ghost_declared(actor, adjacentEpoch)) {
+            if (
+                !handler.ghost_finalized(actor, adjacentEpoch) && !handler.ghost_declared(actor, adjacentEpoch)
+                    && !handler.ghost_validated(actor, adjacentEpoch)
+            ) {
                 assertEq(
                     uint256(registry.presenceState(actor, adjacentEpoch)),
                     uint256(IPresenceRegistry.PresenceState.None),
@@ -221,7 +271,7 @@ contract PresenceRegistryInvariants is Test {
         }
     }
 
-    /// @dev INV6: Ghost consistency (declared and finalized tracking)
+    /// @dev INV6: Ghost consistency (declared, validated, and finalized tracking)
     function invariant_ghostConsistency() external view {
         uint256 len = handler.getGhostActorsLength();
         for (uint256 i = 0; i < len; i++) {
@@ -229,6 +279,7 @@ contract PresenceRegistryInvariants is Test {
             uint256 epochId = handler.getGhostEpoch(i);
 
             bool ghostDeclared = handler.ghost_declared(actor, epochId);
+            bool ghostValidated = handler.ghost_validated(actor, epochId);
             bool ghostFinalized = handler.ghost_finalized(actor, epochId);
             IPresenceRegistry.PresenceState onChainState = registry.presenceState(actor, epochId);
 
@@ -236,9 +287,16 @@ contract PresenceRegistryInvariants is Test {
                 assertEq(
                     uint256(onChainState), uint256(IPresenceRegistry.PresenceState.Finalized), "INV6: Finalized desync"
                 );
+            } else if (ghostValidated) {
+                assertTrue(
+                    onChainState == IPresenceRegistry.PresenceState.Validated
+                        || onChainState == IPresenceRegistry.PresenceState.Finalized,
+                    "INV6: Validated desync"
+                );
             } else if (ghostDeclared) {
                 assertTrue(
                     onChainState == IPresenceRegistry.PresenceState.Declared
+                        || onChainState == IPresenceRegistry.PresenceState.Validated
                         || onChainState == IPresenceRegistry.PresenceState.Finalized,
                     "INV6: Declared desync"
                 );
@@ -249,12 +307,16 @@ contract PresenceRegistryInvariants is Test {
 
 /**
  * @title PresenceRegistryPropertyTests
- * @notice Deterministic property tests
+ * @notice Deterministic property tests (updated for v0.4)
  */
 contract PresenceRegistryPropertyTests is Test {
     IPresenceRegistry internal registry;
     IEpochRegistry internal epochRegistry;
+    IValidatorRegistry internal validatorRegistry;
     address internal constant AUTHORITY = address(0xA077);
+    address internal constant VALIDATOR_1 = address(0x1001);
+    address internal constant VALIDATOR_2 = address(0x1002);
+    address internal constant VALIDATOR_3 = address(0x1003);
 
     address internal constant ACTOR = address(0xA11CE);
     address internal constant ATTACKER = address(0xBEEF);
@@ -262,18 +324,38 @@ contract PresenceRegistryPropertyTests is Test {
 
     function setUp() external {
         epochRegistry = IEpochRegistry(address(new EpochRegistry(AUTHORITY)));
-        registry = IPresenceRegistry(address(new PresenceRegistry(epochRegistry)));
+        validatorRegistry = IValidatorRegistry(address(new ValidatorRegistry(AUTHORITY)));
+        registry = IPresenceRegistry(address(new PresenceRegistry(epochRegistry, validatorRegistry, 0)));
 
         vm.startPrank(AUTHORITY);
         epochRegistry.createEpoch(1, block.timestamp, block.timestamp + 1 days);
         epochRegistry.createEpoch(2, block.timestamp, block.timestamp + 1 days);
+        validatorRegistry.addValidator(VALIDATOR_1);
+        validatorRegistry.addValidator(VALIDATOR_2);
+        validatorRegistry.addValidator(VALIDATOR_3);
         vm.stopPrank();
+    }
+
+    function _declareAndValidate(address actor, uint256 epochId) internal {
+        vm.prank(actor);
+        registry.declarePresence(actor, epochId);
+
+        vm.prank(VALIDATOR_1);
+        registry.validatePresence(actor, epochId);
+        vm.prank(VALIDATOR_2);
+        registry.validatePresence(actor, epochId);
+        vm.prank(VALIDATOR_3);
+        registry.validatePresence(actor, epochId);
     }
 
     function test_singleFinalizedPresencePerEpoch() external {
         assertEq(uint256(registry.presenceState(ACTOR, EPOCH_ID)), uint256(IPresenceRegistry.PresenceState.None));
 
-        vm.prank(ACTOR);
+        _declareAndValidate(ACTOR, EPOCH_ID);
+
+        // Warp to Closed epoch
+        vm.warp(block.timestamp + 2 days);
+
         registry.finalizePresence(ACTOR, EPOCH_ID);
 
         assertEq(uint256(registry.presenceState(ACTOR, EPOCH_ID)), uint256(IPresenceRegistry.PresenceState.Finalized));
@@ -281,20 +363,22 @@ contract PresenceRegistryPropertyTests is Test {
         IPresenceRegistry.PresenceState state = registry.presenceState(ACTOR, EPOCH_ID);
         assertTrue(
             state == IPresenceRegistry.PresenceState.None || state == IPresenceRegistry.PresenceState.Declared
-                || state == IPresenceRegistry.PresenceState.Finalized
+                || state == IPresenceRegistry.PresenceState.Validated || state == IPresenceRegistry.PresenceState.Finalized
+                || state == IPresenceRegistry.PresenceState.Slashed
         );
     }
 
     function test_finalizedPresenceIsImmutable() external {
-        vm.prank(ACTOR);
+        _declareAndValidate(ACTOR, EPOCH_ID);
+
+        // Warp to Closed epoch
+        vm.warp(block.timestamp + 2 days);
+
         registry.finalizePresence(ACTOR, EPOCH_ID);
 
         IPresenceRegistry.PresenceState stateBefore = registry.presenceState(ACTOR, EPOCH_ID);
 
-        vm.prank(ACTOR);
         registry.finalizePresence(ACTOR, EPOCH_ID);
-
-        vm.prank(ACTOR);
         registry.finalizePresence(ACTOR, EPOCH_ID);
 
         IPresenceRegistry.PresenceState stateAfter = registry.presenceState(ACTOR, EPOCH_ID);
@@ -304,11 +388,14 @@ contract PresenceRegistryPropertyTests is Test {
     }
 
     function test_deterministicFinalization() external {
-        vm.prank(ACTOR);
+        _declareAndValidate(ACTOR, EPOCH_ID);
+
+        // Warp to Closed epoch
+        vm.warp(block.timestamp + 2 days);
+
         registry.finalizePresence(ACTOR, EPOCH_ID);
         IPresenceRegistry.PresenceState state1 = registry.presenceState(ACTOR, EPOCH_ID);
 
-        vm.prank(ACTOR);
         registry.finalizePresence(ACTOR, EPOCH_ID);
         IPresenceRegistry.PresenceState state2 = registry.presenceState(ACTOR, EPOCH_ID);
 
@@ -316,16 +403,20 @@ contract PresenceRegistryPropertyTests is Test {
         assertEq(uint256(state1), uint256(IPresenceRegistry.PresenceState.Finalized));
     }
 
-    function test_onlyActorCanFinalizePresence() external {
+    function test_onlyActorCanDeclarePresence() external {
         vm.prank(ATTACKER);
         vm.expectRevert(abi.encodeWithSelector(IPresenceRegistry.UnauthorizedActor.selector, ATTACKER, ACTOR));
-        registry.finalizePresence(ACTOR, EPOCH_ID);
+        registry.declarePresence(ACTOR, EPOCH_ID);
     }
 
     function test_actorIsolation() external {
         address actor2 = address(0xB0B);
 
-        vm.prank(ACTOR);
+        _declareAndValidate(ACTOR, EPOCH_ID);
+
+        // Warp to Closed epoch
+        vm.warp(block.timestamp + 2 days);
+
         registry.finalizePresence(ACTOR, EPOCH_ID);
 
         assertEq(uint256(registry.presenceState(ACTOR, EPOCH_ID)), uint256(IPresenceRegistry.PresenceState.Finalized));
@@ -333,7 +424,11 @@ contract PresenceRegistryPropertyTests is Test {
     }
 
     function test_epochIsolation() external {
-        vm.prank(ACTOR);
+        _declareAndValidate(ACTOR, 1);
+
+        // Warp to Closed epoch
+        vm.warp(block.timestamp + 2 days);
+
         registry.finalizePresence(ACTOR, 1);
 
         assertEq(uint256(registry.presenceState(ACTOR, 1)), uint256(IPresenceRegistry.PresenceState.Finalized));
